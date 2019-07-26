9bd2aaafbe2f4a311103ef9ec9dd337864e5244a
Tom Bickle <tom_bickle@intergral.com>
2019-07-26 09:33:31 +0100

    dk

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const {nerdvision} = require('@nerdvision/agent');
 
 nerdvision.init({
  apiKey : 'cb685576210b8109113c4a3054be99602ff0f1f97cd8f6a7893e5e911ec314202cc88c17b06fe6c1fc806be258cba6def749f92992fda65a7ed59158543e9ee8',
- apiUrl : 'https://api.bbn.nerd.vision'
+ apiUrl : 'api.bbn.nerd.vision'
 }).then( (dks) => {
 
 
