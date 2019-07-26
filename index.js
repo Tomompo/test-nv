9183bbc6092e8b37db543252f0c9a1790464bf79
Tom Bickle <tom_bickle@intergral.com>
2019-07-26 09:01:17 +0100

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const {nerdvision} = require('@nerdvision/agent');
+
+nerdvision.init('cb685576210b8109113c4a3054be99602ff0f1f97cd8f6a7893e5e911ec314202cc88c17b06fe6c1fc806be258cba6def749f92992fda65a7ed59158543e9ee8').then( (dks) => {
+
+
+    console.log('one');
+
+    console.log('two');
+
+    const t = {
+
+    }
+
+    console.log(t.thing.undef);
+
+});
